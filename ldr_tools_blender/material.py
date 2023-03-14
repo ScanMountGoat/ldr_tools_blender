1d46d193c2f45ff5fa292079cf2ef7c86b0a90da
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2023-03-14 17:21:33 -0500

    adjust metal materials

--- ldr_tools_blender/material.py
+++ ldr_tools_blender/material.py
@@ -5,28 +5,32 @@ from .ldr_tools_py import LDrawColor
 import bpy
 
 
-def get_material(color_by_code: dict[int, LDrawColor], color: int):
+def get_material(color_by_code: dict[int, LDrawColor], code: int):
     # Cache materials by name.
     # This loads materials lazily to avoid creating unused colors.
-    ldraw_color = color_by_code[color]
+    ldraw_color = color_by_code.get(code)
 
-    name = f'{color} {ldraw_color.name}'
+    name = f'{code} {ldraw_color.name}' if ldraw_color is not None else str(
+        code)
     material = bpy.data.materials.get(name)
 
-    if material is None:
+    # TODO: Report warnings if a part contains an invalid color code.
+    if material is None and ldraw_color is not None:
         material = bpy.data.materials.new(name)
         material.use_nodes = True
         bsdf = material.node_tree.nodes["Principled BSDF"]
 
         # Alpha is specified using transmission instead.
         r, g, b, a = ldraw_color.rgba_linear
-        bsdf.inputs['Base Color'].default_value = [r, g, b, 1.0]
 
-        metallic = 0.0
-        if ldraw_color.finish_name in ['MatteMetallic', 'Chrome', 'Metal']:
-            metallic = 1.0
-        elif ldraw_color.finish_name == 'Pearlescent':
-            metallic = 0.35
+        # TODO: make a dictionary of color overrides for more realistic colors.
+        if code == 80:
+            # Make Metallic_Silver brighter
+            r *= 3.0
+            g *= 3.0
+            b *= 3.0
+
+        bsdf.inputs['Base Color'].default_value = [r, g, b, 1.0]
 
         # Transparent colors specify an alpha of 128 / 255.
         is_transmissive = a <= 0.6
@@ -35,10 +39,10 @@ def get_material(color_by_code: dict[int, LDrawColor], color: int):
         bsdf.inputs['Subsurface Radius'].default_value = [
             0.001, 0.001, 0.001]  # TODO: should depend on scene scale
         bsdf.inputs['Subsurface'].default_value = 1.0
-        bsdf.inputs['Metallic'].default_value = metallic
 
         # Procedural roughness.
-        roughness_node = create_node_group(material, 'ldr_tools_roughness', create_roughness_node_group)
+        roughness_node = create_node_group(
+            material, 'ldr_tools_roughness', create_roughness_node_group)
 
         material.node_tree.links.new(
             roughness_node.outputs['Roughness'], bsdf.inputs['Roughness'])
@@ -46,6 +50,47 @@ def get_material(color_by_code: dict[int, LDrawColor], color: int):
         # Normal opaque materials.
         roughness_node.inputs['Min'].default_value = 0.075
         roughness_node.inputs['Max'].default_value = 0.2
+
+        # TODO: Have a case for each finish type?
+        if ldraw_color.finish_name == 'MatteMetallic':
+            bsdf.inputs['Metallic'].default_value = 1.0
+        if ldraw_color.finish_name == 'Chrome':
+            # Glossy metal coating.
+            bsdf.inputs['Metallic'].default_value = 1.0
+            roughness_node.inputs['Min'].default_value = 0.075
+            roughness_node.inputs['Max'].default_value = 0.1
+        if ldraw_color.finish_name == 'Metal':
+            # Rougher metals.
+            bsdf.inputs['Metallic'].default_value = 1.0
+            roughness_node.inputs['Min'].default_value = 0.15
+            roughness_node.inputs['Max'].default_value = 0.3
+        elif ldraw_color.finish_name == 'Pearlescent':
+            bsdf.inputs['Metallic'].default_value = 0.35
+            roughness_node.inputs['Min'].default_value = 0.3
+            roughness_node.inputs['Max'].default_value = 0.5
+        elif ldraw_color.finish_name == 'Speckle':
+            # TODO: Are all speckled colors metals?
+            bsdf.inputs['Metallic'].default_value = 1.0
+
+            speckle_node = create_node_group(
+                material, 'ldr_tools_speckle', create_speckle_node_group)
+
+            # Adjust the thresholds to control speckle size and density.
+            speckle_node.inputs['Min'].default_value = 0.5
+            speckle_node.inputs['Max'].default_value = 0.6
+
+            # Blend between the two speckle colors.
+            mix_rgb = material.node_tree.nodes.new('ShaderNodeMixRGB')
+
+            material.node_tree.links.new(
+                speckle_node.outputs['Fac'], mix_rgb.inputs['Fac'])
+            mix_rgb.inputs[1].default_value = [r, g, b, 1.0]
+            speckle_r, speckle_g, speckle_b, _ = ldraw_color.speckle_rgba_linear
+            mix_rgb.inputs[2].default_value = [
+                speckle_r, speckle_g, speckle_b, 1.0]
+
+            material.node_tree.links.new(
+                mix_rgb.outputs['Color'], bsdf.inputs['Base Color'])
 
         if is_transmissive:
             bsdf.inputs['Transmission'].default_value = 1.0
@@ -64,31 +109,14 @@ def get_material(color_by_code: dict[int, LDrawColor], color: int):
             # Disable shadow casting for transparent materials.
             # This avoids making transparent parts too dark.
             make_shadows_transparent(material, bsdf)
-        elif ldraw_color.finish_name == 'Speckle':
-            # TODO: Are all speckled colors metals?
-            bsdf.inputs['Metallic'].default_value = 1.0
-
-            speckle_node = create_node_group(material, 'ldr_tools_speckle', create_speckle_node_group)
-
-            # Adjust the thresholds to control speckle size and density.
-            speckle_node.inputs['Min'].default_value = 0.5
-            speckle_node.inputs['Max'].default_value = 0.6
-
-            # TODO: Blend between the two speckle colors.
-            mix_rgb = material.node_tree.nodes.new('ShaderNodeMixRGB')
-
-            material.node_tree.links.new(speckle_node.outputs['Fac'], mix_rgb.inputs['Fac'])
-            mix_rgb.inputs[1].default_value = [r, g, b, 1.0]
-            speckle_r, speckle_g, speckle_b, _ = ldraw_color.speckle_rgba_linear
-            mix_rgb.inputs[2].default_value = [speckle_r, speckle_g, speckle_b, 1.0]
-
-            material.node_tree.links.new(mix_rgb.outputs['Color'], bsdf.inputs['Base Color'])
 
         # Procedural normals.
-        normals_node = create_node_group(material, 'ldr_tools_normal', create_normals_node_group)
+        if not is_transmissive:
+            normals_node = create_node_group(
+                material, 'ldr_tools_normal', create_normals_node_group)
 
-        material.node_tree.links.new(
-            normals_node.outputs['Normal'], bsdf.inputs['Normal'])
+            material.node_tree.links.new(
+                normals_node.outputs['Normal'], bsdf.inputs['Normal'])
 
         # Set the color in the viewport.
         material.diffuse_color = [r, g, b, 1.0]
@@ -205,14 +233,14 @@ def create_normals_node_group(name: str) -> bpy.types.NodeTree:
 
     # TODO: Create frame called "unevenness" or at least name the nodes.
     unevenness_noise = inner_nodes.new('ShaderNodeTexNoise')
-    unevenness_noise.inputs['Scale'].default_value = 5.0
+    unevenness_noise.inputs['Scale'].default_value = 3.0
     unevenness_noise.inputs['Detail'].default_value = 0.0
     unevenness_noise.inputs['Roughness'].default_value = 0.0
     unevenness_noise.inputs['Distortion'].default_value = 0.0
 
     unevenness_bump = inner_nodes.new('ShaderNodeBump')
-    unevenness_bump.inputs['Strength'].default_value = 0.15
-    unevenness_bump.inputs['Distance'].default_value = 0.15
+    unevenness_bump.inputs['Strength'].default_value = 0.1
+    unevenness_bump.inputs['Distance'].default_value = 0.1
 
     # TODO: Create frame called "micronoise" or at least name the nodes.
     micro_noise = inner_nodes.new('ShaderNodeTexNoise')
